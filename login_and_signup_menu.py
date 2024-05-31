325b76262e007e3f9b7aa6052a8be5ca95b1286e
unknown <mani.masteali7@gmail.com>
2024-06-01 01:59:50 +0330

    signup is completed but needs debugging. sign up is added to the menu but needs some changes in the main function. a csv file created to store users.

--- login_and_signup_menu.py
+++ login_and_signup_menu.py
@@ -1,19 +1,37 @@
 from colors import blue,green
 from rich.console import Console
 from rich.text import Text
-from signup import *
+from signup import User
 
 class FirstMenu():
     def __init__(self):
         self.menu_options=['sign up','log in']
     def show_menu(self):
-        Console.clear()
-        Console.print(Text('Welcome to the accounting managment app !',style=blue))
-        Console.print(Text('please choose whether you want to sign up or log in by parsing using the arrow keys or typing the option you want'),style=green)
+        Console().clear()
+        Console().print(Text('Welcome to the accounting managment app !',style=blue))
+        Console().print(Text('please choose whether you want to sign up or log in by parsing using the arrow keys or typing the option you want'),style=green)
     def handle_input(self,input):
-        if 
+        pass
 
 if __name__=='__main__':
-    firstMenu=FirstMenu.show_menu()
+    menu=FirstMenu()
+    menu.show_menu()
+    option=input()
+    if option=='sign up':
+        while True:
+            userX=User()
+            userX.get_first_name(input('first name: '))
+            userX.get_last_name(input('last name: '))
+            userX.get_code_meli(input('national id: '))
+            userX.get_phone_number(input('mobile number: '))
+            userX.get_username(input('user name: '))
+            userX.get_password(input('password: '))
+            userX.check_repeated_password(input('confirm password: '))
+            userX.get_city(str(Console().input(f'[bold white] please choose the city from this list: [cyan] {userX.savedcities}')))
+            userX.get_email(input('email: '))
+            userX.get_birth_date(input('birth date: (yyyy/mm/dd) '))
+            userX.get_security_questions_answer(input('What is your favorite car brand? '))
+            userX.save_csv()
+            break
 
 
--- signup.py
+++ signup.py
@@ -1,7 +1,8 @@
-from colors import *
+from colors import * 
 from rich.console import Console
 from rich.text import Text
 import re
+import pandas
 # در اینجا اطلاعات مربوط به کاربر به صورت یک شی از کلاس یوزر ساخته و تعریف می شود
 class User:
     def __init__(self):
@@ -11,44 +12,113 @@ class User:
         self.phoneNumber=None
         self.userName=None
         self.password=None
-        self.repeatedPassWord=None
         self.city=None
         self.email=None
         self.birthDate=None
         self.securityQAnswer=None
+        self.savedcities=['Tehran','Isfahan','Mashhad','Tabriz','Shiraz','Karaj','Qom','Yazd','Rasht','Sari']
     # بررسی معتبر بودن اسم کوچک و تعریف آن برای کاربر
     def get_first_name(self,firstName):
         if len(re.findall('[a-z]',firstName))+len(re.findall('[A-Z]',firstName)) == len(firstName):
             self.firstName=firstName
         else:
-            raise ValueError(Console.print('first name must only consist of english letters'),style=red)
+            raise ValueError(Console().print('[bold red]first name must only consist of English letters.'))
     # بررسی معتبر بودن نام خانوادگی و تعریف آن برای کاربر
     def get_last_name(self,lastName):
         if len(re.findall('[a-z]',lastName))+len(re.findall('[A-Z]',lastName)) == len(lastName):
             self.lastName=lastName
         else:
-            raise ValueError(Console.print('last name must only consist of english letters'),style=red)
+            raise ValueError(Console().print('[bold red]last name must only consist of English letters.'))
     #بررسی معتبر بودن کد ملی و تعریف آن برای کاربر
     def get_code_meli(self,nationalId):
         if len(nationalId)==10 :
             if len(re.findall('[0-9]',nationalId))==len(nationalId):
                 self.nationalId=nationalId
             else:
-                Console.print('National ID only has numbers in it',style=red)
+                ValueError(Console().print('[bold red]National ID only has numbers in it.'))
         else:
-            raise ValueError(Console.print('National ID must have ten numbers',style=red))
+            raise ValueError(Console().print('[bold red]National ID must have ten numbers.'))
     #بررسی معتبر بودن شماره تلفن و تعریف آن برای کاربر
     def get_phone_number(self,phoneNumber):
         if len(phoneNumber)==11 and len(re.findall('[0-9]',phoneNumber))==len(phoneNumber) and  bool(re.search('^09.',phoneNumber))==True:
             self.phoneNumber=phoneNumber
         elif len(phoneNumber)!=11:
-            Console.print('phone number must have eleven digits',style=red)
+            raise ValueError(Console().print('[bold red]phone number must have eleven digits.'))
         elif len(re.findall('[0-9]',phoneNumber))!=len(phoneNumber):
-            Console.print('phone number must only consist of digits',style=red)
+            raise ValueError(Console().print('[bold red]phone number must only consist of digits.'))
         elif bool(re.search('^09.',phoneNumber))==False:
-            Console.print('phone number must begin with 09',style=red)
+            ValueError(Console().print('[bold red]phone number must begin with 09.'))
     #گرفتن یوزرنیم از کاربر  که می تواند به هر شکل دلخواه باشد.
     def get_username(self,userName):
         self.userName=userName
+    #گرفتن پسورد در صورتی که شرایط دلخواه مسئله را رعایت کند
+    def get_password(self,password):
+        if len(password)>=6 and len(re.findall('[a-z]',password))>=1 and len(re.findall('[A-Z]',password))>=1 and len(re.findall("[!#$%&'()*+,-./:;<=>?@[\]^_`{|}~]",password))>=1 and len(re.findall('[0-9]',password))>=1:
+            self.password=password
+        elif len(password)<6:
+            raise ValueError(Console().print('[bold red]password must be at least 6 characters long.'))
+        elif len(re.findall('[a-z]',password))<1:
+            raise ValueError(Console().print('[bold red]password must contain at least one lowercase letter.'))
+        elif len(re.findall('[A-Z]',password))<1:
+            raise ValueError(Console().print('[bold red]password must contain at least one uppercase letter.'))
+        elif len(re.findall("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"),password)<1:
+            raise ValueError(Console().print('[bold red]password must contain at least one digit.'))
+        elif len(re.findall('[0-9]',password))<1:
+            raise ValueError(Console().print('[bold red]password must contain at least one special character (!#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~).'))
+    #برای تایید رمز عبور  
+    def check_repeated_password(self,repeatedPassword):
+        if repeatedPassword!=self.password:
+            Console.print('passwords do not match',style=red)
+    # گرفتن نام شهر از کاربر در صورتی که در لیست تعریف شده باشد
+    def get_city(self,city):
+        if city in self.savedcities:
+            self.city=city
+        else:
+            raise ValueError(Console().print(f'[bold red]Invalid city. Please choose the city from this list: [cyan]{self.savedcities}.'))
+    # بررسی معتبر بودن ایمیل و گرفتن از کاربر
+    def get_email(self,email):
+        if bool(re.findall(r'[A-Za-z0-9]+@(gmail|yahoo)\.com',email))==True:
+            self.email=email
+        else:
+            Console().print('invalid email',style=red)
+    def get_birth_date(self,birthDate):
+        if bool(re.findall('[1-2][0-9][0-9][0-9]/[0-1][0-9]/[0-3][0-9]',birthDate))==True:
+            year=birthDate[0:4]
+            month=birthDate[5:7]
+            day=birthDate[8:10]
+            if int(year)>=1920 and int(year)<=2005 and 1<=int(month)<=12:
+                max_days={1:31,2:29,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
+                if 1<=int(day)<=max_days[int(month)]:
+                    self.birthDate=birthDate
+                else:
+                    Console().print(f'invalid day. this month has only {max_days[month]}',style=red)
+            elif int(year)<1920 or int(year)>2005:
+                Console().print('birth year must be between 1920 and 2005',style=red)
+            elif int(month)<1 or int(month)>12:
+                Console().print('month must be between 1 and 12',style=red)
+        else:
+            Console().print('invalid birth date format',style=red)
+    #گرفتن پاسخ سوال امنیتی که از کاربر پرسیده می شود
+    def get_security_questions_answer(self,answer):
+        self.securityQAnswer=answer
+    #اطلاعات کاربر در یک فایل سی اس وی ذخیره می شود
+    def save_csv(self):
+        users=pandas.read_csv('users.csv')
 
+        new_user={
+            'firstName':[self.firstName],
+            'lastName':[self.lastName],
+            'nationalId':[self.nationalId],
+            'phoneNumber':[self.phoneNumber],
+            'username':[self.userName],
+            'password':[self.password],
+            'city':[self.city],
+            'email':[self.email],
+            'birthDate':[self.birthDate],
+            'securityQAnswer':[self.securityQAnswer]
+        }
+        new_user=pandas.DataFrame(new_user)
+        users=pandas.concat([users,new_user])
+        users.to_csv('users.csv',index=False)
+        
     
