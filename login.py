995e9e767dea3e11298d75862413bd30c2377bd3
unknown <mani.masteali7@gmail.com>
2024-06-01 23:50:02 +0330

    login menu was created successfully. but it of course needs some final debugs. the add of login has also changed login_and_signup_menu.py

---
+++ login.py
@@ -0,0 +1,37 @@
+from signup import User
+from rich.console import Console
+from rich.text import Text
+import time
+import pandas
+from colors import green,red
+
+def login_user(userName, passWord):
+    users = pandas.read_csv('users.csv')
+    for _, user in users.iterrows():
+        if user['userName'] == userName and user['password'] == passWord:
+            return True
+    return False
+def count_wrong_enters(count):
+    if count%3==0:
+        Console.print(Text('try again in one minute!'),style=red)
+        time.sleep(60)
+    else:
+        pass
+def forgot_password(userNameOrEmail,securityQAnswer):
+    users = pandas.read_csv('users.csv')
+    users['userName'] = users['userName'].astype(str).str.lower()
+    users['email'] = users['email'].astype(str).str.lower()
+    matching_users = users[
+        (users['userName'] == userNameOrEmail.lower()) |
+        (users['email'] == userNameOrEmail.lower())
+    ]
+    if not matching_users.empty:
+        user = matching_users.iloc[0]  
+        if user['SecuityQAnswer'] == securityQAnswer:
+            Console().print(Text(f"Your password is: {user['password']}"), style=green)
+        else:
+            Console().print(Text("Security question answer is wrong!"), style=red)
+    else:
+        Console().print(Text("Username or email does not exist!"), style=red)
+
+
--- login_and_signup_menu.py
+++ login_and_signup_menu.py
@@ -1,7 +1,9 @@
-from colors import blue,green
+from colors import green,blue,cyan,red
 from rich.console import Console
 from rich.text import Text
 from signup import User
+import getpass
+from login import login_user,count_wrong_enters,forgot_password
 
 class FirstMenu():
     def __init__(self):
@@ -9,15 +11,15 @@ class FirstMenu():
     def show_menu(self):
         Console().clear()
         Console().print(Text('Welcome to the accounting managment app !',style=blue))
-        Console().print(Text('please choose whether you want to sign up or log in by parsing using the arrow keys or typing the option you want'),style=green)
-    def handle_input(self,input):
-        pass
+        Console().print(Text('please choose whether your option'),style=green)
+        Console().print(Text('1- sign up'),style=cyan)
+        Console().print(Text('2- log in'),style=cyan)
 
 if __name__=='__main__':
     menu=FirstMenu()
     menu.show_menu()
     option=input()
-    if option=='sign up':
+    if option=='1':
         while True:
             userX=User()
             try:
@@ -26,15 +28,37 @@ if __name__=='__main__':
                 userX.get_code_meli(input('national id: '))
                 userX.get_phone_number(input('mobile number: '))
                 userX.get_username(input('user name: '))
-                userX.get_password(input('password: '))
-                userX.check_repeated_password(input('confirm password: '))
+                userX.get_password(getpass.getpass('enter password:'))
+                userX.check_repeated_password(getpass.getpass('confrim password:'))
                 userX.get_city(str(Console().input(f'[bold white] please choose the city from this list: [cyan] {userX.savedcities} \n :')))
                 userX.get_email(input('email: '))
                 userX.get_birth_date(input('birth date: (yyyy/mm/dd) '))
                 userX.get_security_questions_answer(input('What is your favorite car brand? '))
                 userX.save_csv()
-                break
+                last_option=str(Console().input('[green] type submit to continue'))
+                if last_option=='submit':
+                    break
+                else:
+                    Console().print('[bold red] Invalid input')
             except ValueError as error:
                 print(error)
+    elif option=='2':
+        count=0
+        while True:
+            userName=input('username: ')
+            password=getpass.getpass('password: ')
+            if login_user(userName,password)==True:
+                Console().print(Text('login was succesful!'),style=green)
+                break
+            else:
+                Console().print(Text('username or password is wrong or does not exist!'),style=red)
+                count+=1
+                count_wrong_enters(count)
+                forgotOrNot=input('forgot your password? ')
+                if forgotOrNot=='Yes' or forgotOrNot=='yes':
+                    userNameOrEmail=input('enter your username or email: ')
+                    securityQAnswer=input('What is your favorite car brand? ')
+                    forgot_password(userNameOrEmail,securityQAnswer)
+
 
 
