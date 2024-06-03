92af885cb695318131f15fcd5952473d22ed3f36
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-06-03 19:51:41 +0330

    Added import statement, save_categories method, add_category method, and is_category_valid method to Category class. Now i must check how the program works and make change, make files and complete menu.

--- category.py
+++ category.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Category:
     def __init__(self, file="categories.txt"):
         self.file = file
@@ -9,3 +12,32 @@ class Category:
                 return [line.strip() for line in f if line.strip()]
         except FileNotFoundError:
             return []
+
+    def save_categories(self):
+        with open(self.file, mode="w") as f:
+            for cat in self.categoriesList:
+                f.write(cat+"\n")
+
+    def add_category(self):
+        while True:
+            category = input("Enter new category name: ")
+            if self.is_category_valid(category):
+                if category in self.categoriesList:
+                    print(f"Category {category} already exists.")
+                else:
+                    self.categoriesList.append(category)
+                    self.save_categories()
+                    print(f"Category {category} added successfully.")
+                break
+
+    def is_category_vailid(self, category):
+        if not category:
+            print("Category name cannot be empty.")
+            return False
+        if len(category) > 15:
+            print("Category name must be 15 characters or less.")
+            return False
+        if not re.match("^[A-Za-z0-9]+$", category):
+            print("Category name must contain only letters and numbers.")
+            return False
+        return True
